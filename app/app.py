e2dd4e47c7f14a362a6dcc86906029b5a7aa61b6
Emmanuel <emmanuellekan30@gmail.com>
2026-08-11 01:03:10 +0100

    adjusted

--- app/app.py
+++ app/app.py
@@ -37,14 +37,10 @@ def ensure_user_schema():
 
 
 def create_app():
-    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-    template_dir = os.path.join(base_dir, 'templates')
-    static_dir = os.path.join(base_dir, 'static')
-
     app = Flask(
         __name__,
-        template_folder=template_dir,
-        static_folder=static_dir,
+        template_folder='../templates',
+        static_folder='../static'
     )
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///devdb.db'
